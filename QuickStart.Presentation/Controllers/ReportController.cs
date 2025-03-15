1c7f0c7b47f4becc95f848228951451f1f0059d4
Trần Đức Hải <haitran.qng@outlook.com>
2025-03-15 09:41:13 +0700

    fix database

---
+++ QuickStart.Presentation/Controllers/ReportController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QuickStart.Presentation.ActionFilters;
+using Service.Contracts;
+using Shared.DataTransferObjects.Area;
+
+namespace QuickStart.Presentation.Controllers
+{
+    [Route("api/reports")]
+    [ApiController]
+    //[Authorize]
+    public class ReportController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public ReportController(IServiceManager service) => _service = service;
+
+    }
+}
